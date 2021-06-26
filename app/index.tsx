578aa4cdf8c10a5ee90ba4215f3a798266f93422
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-06-25 18:26:35 -0700

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Example from "./example";
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+
+const root = document.createElement("div");
+root.setAttribute("id", "root");
+document.body.appendChild(root);
+
+/**
+ * Primary render function for app. Called on store updates
+ */
+ReactDOM.render(
+    <DndProvider backend={HTML5Backend}>
+        <Example />
+    </DndProvider>,
+    root
+);
